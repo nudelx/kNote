b67f2b9953c39d47f14de3f8c790ab1e747d8d28
Alex Nudelman <nudelx@gmail.com>
2016-11-01 23:03:59 +0200

    done with test

--- JS/promises.js
+++ JS/promises.js
@@ -1,12 +1,11 @@
 var url = 'http://swapi.co/api/films/';
 var cb  = function () { console.log('I am a callback !!', arguments)}
 
-var getFilmsWithCallback = function (url) {
+var getFilmsWithPromise = function (url) {
   return new Promise(function (yes, no) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-        debugger
+      if (xhr.readyState == XMLHttpRequest.DONE && this.status === 200) {
           yes(xhr.response)
       } else {
           no("jopa")
@@ -19,39 +18,49 @@ var getFilmsWithCallback = function (url) {
 
 
 var getFilmsWithCallback = function (url,cb) {
-  return new Promise(function (yes, no) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      if (xhr.readyState == XMLHttpRequest.DONE && this.status === 200) {
-        if( typeof cb === 'function') { cb(xhr.response) }
-          else { console.warn("No callback found")}
-      } else {
-          console.error("XHR error "+ this.status + " " + xhr.statusText )
-      }
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE && this.status === 200) {
+      if( typeof cb === 'function') { cb(xhr.response) }
+        else { console.warn("No callback found")}
+    } else {
+        console.error("XHR error "+ this.status + " " + xhr.statusText )
     }
-
-    xhr.open('GET', url, true);
-    xhr.send(null);
-  })
+  }
+  xhr.open('GET', url, true);
+  xhr.send(null);
 }
 
-var t = [ { url, cb }, { url: url+'dddddd', cb }, {url} ].map((item) => { getFilmsWithCallback(item.url, item.cb) })
+function theBadAndUgly () {
+  var t = [ { url, cb }, { url: url+'dddddd', cb }, {url} ].map((item) => { getFilmsWithCallback(item.url, item.cb) })
 
-getFilmsWithCallback(url+"1", function(text1) {
-  console.log("one")
-  getFilmsWithCallback(url+"2", function(text2) {
-    console.log("two")
-    getFilmsWithCallback(url+"3", function(text3) {
-      console.log("tree")
-      getFilmsWithCallback(url+"4", function(text4) {
-        console.log( "Yes !!! we got the movies !!!  => "
-        + JSON.parse(text1).title + " | "
-        + JSON.parse(text2).title + " | "
-        + JSON.parse(text3).title + " | "
-        + JSON.parse(text4).title
-        //TODO :: add error handeling :)  
-       )
+  getFilmsWithCallback(url+"1", function(text1) {
+    console.log("one")
+    getFilmsWithCallback(url+"2", function(text2) {
+      console.log("two")
+      getFilmsWithCallback(url+"3", function(text3) {
+        console.log("tree")
+        getFilmsWithCallback(url+"4", function(text4) {
+          console.log( "Yes !!! we got the movies !!!  => "
+          + JSON.parse(text1).title + " | "
+          + JSON.parse(text2).title + " | "
+          + JSON.parse(text3).title + " | "
+          + JSON.parse(text4).title
+          //TODO :: add error handeling :)
+         )
+        })
       })
     })
   })
+
+}
+
+var t = [ { url}, { url: url+'dddddd'} ].map((item) => {
+  getFilmsWithPromise(item.url).then(function(){ console.log("data !!!")}).catch(function(){ console.log("error !!!")})
 })
+
+
+//
+// var whenDataLoaded = getFilmsWithPromise(url)
+//
+// whenDataLoaded.then(function(data) { console.log(data)})
