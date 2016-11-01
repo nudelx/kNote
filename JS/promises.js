12397513e334e238358c835baf78c5dce61372f5
Alex Nudelman <nudelx@gmail.com>
2016-11-01 22:40:06 +0200

    example with callback

--- JS/promises.js
+++ JS/promises.js
@@ -1,15 +1,38 @@
-console.log("heheh");
-var api = 'http://swapi.co/api/films/';
+var url = 'http://swapi.co/api/films/';
+var cb  = function () { console.log('I am a callback !!', arguments)}
 
-
-var getFilms = function (url) {
+var getFilmsWithCallback = function (url) {
   return new Promise(function (yes, no) {
-
-    var res = window.fetch(url).then(function(res) {return res.json()} ).then(function(res) { return res});
-
-
-
-
-  } )
-
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        debugger
+          yes(xhr.response)
+      } else {
+          no("jopa")
+      }
+    }
+    xhr.open('GET', url, true);
+    xhr.send(null);
+  })
 }
+
+
+var getFilmsWithCallback = function (url,cb) {
+  return new Promise(function (yes, no) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE && this.status === 200) {
+        if( typeof cb === 'function') { cb(xhr.response) }
+          else { console.warn("No callback found")}
+      } else {
+          console.error("XHR error "+ this.status + " " + xhr.statusText )
+      }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send(null);
+  })
+}
+
+var t = [ { url, cb }, { url: url+'dddddd', cb }, {url} ].map((item) => { getFilmsWithCallback(item.url, item.cb) })
