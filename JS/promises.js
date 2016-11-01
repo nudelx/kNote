8ad57d5b04075a699d549e921e25800ca33f0e79
Alex Nudelman <nudelx@gmail.com>
2016-11-01 22:53:08 +0200

    done with xmas tree examples

--- JS/promises.js
+++ JS/promises.js
@@ -36,3 +36,22 @@ var getFilmsWithCallback = function (url,cb) {
 }
 
 var t = [ { url, cb }, { url: url+'dddddd', cb }, {url} ].map((item) => { getFilmsWithCallback(item.url, item.cb) })
+
+getFilmsWithCallback(url+"1", function(text1) {
+  console.log("one")
+  getFilmsWithCallback(url+"2", function(text2) {
+    console.log("two")
+    getFilmsWithCallback(url+"3", function(text3) {
+      console.log("tree")
+      getFilmsWithCallback(url+"4", function(text4) {
+        console.log( "Yes !!! we got the movies !!!  => "
+        + JSON.parse(text1).title + " | "
+        + JSON.parse(text2).title + " | "
+        + JSON.parse(text3).title + " | "
+        + JSON.parse(text4).title
+        //TODO :: add error handeling :)  
+       )
+      })
+    })
+  })
+})
