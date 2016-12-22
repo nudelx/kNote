85de36e64e5c1d182588dd7a5c4e484402729ed1
nudelx <alex.nudelman@samanage.com>
2016-12-22 15:47:56 +0200

    added hour calc

--- JS/clock/script.js
+++ JS/clock/script.js
@@ -12,9 +12,9 @@ setInterval(function () {
 
     ds.style.transform= `rotate(${90+(sec/60)*360}deg)`
     dm.style.transform= `rotate(${90+(min/60)*360}deg)`
-    dh.style.transform= `rotate(${90+(hour/12)*360}deg)`
+    dh.style.transform= `rotate(${90+((hour+(min)/100)/12)*360}deg)`
     if (sec === 0) { ds.style.transitionDuration = '0.001s'}
     if (sec === 0) { dm.style.transitionDuration = '0.001s'}
     if (sec === 0) { dh.style.transitionDuration = '0.001s'}
-    
+
 }, 1000)
